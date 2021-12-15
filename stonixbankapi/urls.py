58a8d12a2200d1cbb08f1671fa6900826034db66
Livingston-k <kaddulivingston@gmail.com>
2021-12-16 00:27:24 +0300

    advace

--- stonixbankapi/urls.py
+++ stonixbankapi/urls.py
@@ -1,8 +1,9 @@
 
 from django.contrib import admin
 from django.urls import path,include
-
+from rest_framework.authtoken import views
 urlpatterns = [
+    path('login/', views.obtain_auth_token),
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
 ]
