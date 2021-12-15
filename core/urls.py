576b6b23db98b99dc10fe626a9bad8d0ba47092f
Livingston-k <kaddulivingston@gmail.com>
2021-12-15 08:19:11 +0300

    transactions

--- core/urls.py
+++ core/urls.py
@@ -3,7 +3,10 @@ from core import views as CoreViews
 from rest_framework import routers
 
 router = routers.SimpleRouter()
-router.register(r'categories', CoreViews.CategoryModelViewSet,basename='category')
+router.register(r'categories', CoreViews.CategoryModelViewSet,
+                basename='category')
+router.register(r'transactions', CoreViews.TransactionModelViewSet,
+                basename='transactions')
 
 urlpatterns = [
     path('currencies/', CoreViews.CurrenceListApiView.as_view(), name='currencies'),
--- core/views.py
+++ core/views.py
@@ -1,16 +1,28 @@
 from django.db.models import query
 from django.shortcuts import render
-from core.models import Currency,Category
+from core.models import Currency, Category, Transaction
 from rest_framework import generics
 from rest_framework import viewsets
-from core.serializers import CurrencySerializer,CategorySerializer
+from core.serializers import CurrencySerializer, CategorySerializer, WriteTransactionSerializer, ReadTransactionSerializer
 # Create your views here.
 
 class CurrenceListApiView(generics.ListAPIView):
     queryset = Currency.objects.all()
     serializer_class = CurrencySerializer
+    pagination_class = None
 
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class TransactionModelViewSet(viewsets.ModelViewSet):
+    queryset = Transaction.objects.select_related('currency','category')
+    serializer_class = ReadTransactionSerializer
+
+    def get_serializer_class(self):
+        if self.action in ['list','retrieve']:
+            return ReadTransactionSerializer
+        return WriteTransactionSerializer
+
